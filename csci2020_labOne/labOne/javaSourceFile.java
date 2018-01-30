10268ab5d5534b7311743c0daa217c1e863cd976
Colin Orian <colin.orian@uoit.net>
2018-01-30 08:19:23 -0500

    Moved to linux

---
+++ csci2020_labOne/labOne/javaSourceFile.java
@@ -0,0 +1,8 @@
+public class javaSourceFile{
+  public static void main(String[] args){
+    System.out.print("Hello World");
+    System.out.println("Testing cool stuff");
+    System.out.println("More testing.");
+  }
+
+}
